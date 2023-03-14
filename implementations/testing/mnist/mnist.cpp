0f6602223fd3cb44bc820fcda1b84dd002593dc4
William Denny <wdenny1999@gmail.com>
2023-03-14 11:48:19 +0000

    Fixed MNIST import issue, now adding NN into class

--- implementations/testing/mnist/mnist.cpp
+++ implementations/testing/mnist/mnist.cpp
@@ -6,6 +6,19 @@
 
 #include "../../neural_network.hpp"
 
+// prints vectors
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec)
+{
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        if (i > 0)
+            os << ", ";
+        os << vec[i];
+    }
+    return os;
+}
+
 // reverses ordering of bytes in word (needed for IDX file formatting)
 template <typename T>
 T swap(T word)
@@ -22,6 +35,7 @@ T swap(T word)
     return swapped;
 }
 
+// import data from IDX file
 template <typename T>
 std::tuple<T*, std::vector<uint32_t>, uint32_t, uint32_t> read_IDX(const std::string& path)
 {
@@ -41,6 +55,9 @@ std::tuple<T*, std::vector<uint32_t>, uint32_t, uint32_t> read_IDX(const std::st
     buf.read((char*)(&magic_num), 4);
     num_dims = (swap(magic_num)) & 0xff;
     dtype_code = (swap(magic_num) >> 8) & 0xff;
+
+    //std::cout << std::bitset<32>(magic_num) << std::endl;
+    //std::cout << std::bitset<32>(swap(magic_num)) << std::endl;
 
     std::cout << "IDX IMPORT: " << path << "\nDtype code: " << dtype_code << " | Number of dims: " << num_dims << " | Size: (";
     
@@ -78,30 +95,168 @@ private:
     std::vector<std::vector<double>> m_test_x;
     std::vector<std::vector<double>> m_test_y;
 
+    neural_network* m_nn;
+
 public:
     MNIST() = delete;
     MNIST(const std::string& p_train_imgs, 
         const std::string& p_train_labels, 
         const std::string& p_test_imgs, 
-        const std::string& p_test_labels);
+        const std::string& p_test_labels,
+        const std::vector<int>& shape,
+        const std::vector<std::string>& activate_types,
+        const std::string& loss_type);
+    ~MNIST();
 
     void train();
 
-    void draw(const int& set, const int& n);
+    void print_char(const std::string& set, const int& n);
+
+    void eval(const std::string& set, const int& n);
 
 };
 
+// ctor
 MNIST::MNIST(const std::string& p_train_imgs, 
     const std::string& p_train_labels, 
     const std::string& p_test_imgs, 
-    const std::string& p_test_labels)
+    const std::string& p_test_labels,
+    const std::vector<int>& shape,
+    const std::vector<std::string>& activate_types,
+    const std::string& loss_type)
 {
+    std::tuple<uint8_t*, std::vector<uint32_t>, uint32_t, uint32_t> train_imgs_tup;
+    std::tuple<uint8_t*, std::vector<uint32_t>, uint32_t, uint32_t> train_labels_tup;
+    std::tuple<uint8_t*, std::vector<uint32_t>, uint32_t, uint32_t> test_imgs_tup;
+    std::tuple<uint8_t*, std::vector<uint32_t>, uint32_t, uint32_t> test_labels_tup;
+
+    train_imgs_tup   = read_IDX<uint8_t>(p_train_imgs);
+    train_labels_tup = read_IDX<uint8_t>(p_train_labels);
+    test_imgs_tup    = read_IDX<uint8_t>(p_test_imgs);
+    test_labels_tup  = read_IDX<uint8_t>(p_test_labels);
+
+    uint8_t* train_imgs_arr   = std::get<0>(train_imgs_tup);
+    uint8_t* train_labels_arr = std::get<0>(train_labels_tup);
+    uint8_t* test_imgs_arr    = std::get<0>(test_imgs_tup);
+    uint8_t* test_labels_arr  = std::get<0>(test_labels_tup);
+
+    // allocate vectors
+    m_train_x = std::vector<std::vector<double>>(60000);
+    m_train_y = std::vector<std::vector<double>>(60000);
+    m_test_x = std::vector<std::vector<double>>(10000);
+    m_test_y = std::vector<std::vector<double>>(10000);
+
+    for (int i = 0; i < 60000; ++i)
+    {
+        m_train_x[i] = std::vector<double>(28*28);
+        m_train_y[i] = std::vector<double>(10, 0.0);
+    }
+    for (int i = 0; i < 10000; ++i)
+    {
+        m_test_x[i] = std::vector<double>(28*28);
+        m_test_y[i] = std::vector<double>(10, 0.0);
+    }
+
+    // assign data in vectors
+    for (int i = 0; i < 60000; ++i)
+    {
+        for (int j = 0; j < 28*28; ++j)
+        {
+            m_train_x[i][j] = ((double)train_imgs_arr[28*28*i + j]) / 255.0;
+        }
+        m_train_y[i][train_labels_arr[i]] = 1.0;
+    }
+    for (int i = 0; i < 10000; ++i)
+    {
+        for (int j = 0; j < 28*28; ++j)
+        {
+            m_test_x[i][j] = ((double)train_imgs_arr[28*28*i + j]) / 255.0;
+        }
+        m_test_y[i][test_labels_arr[i]] = 1.0;
+    }
+
+    // initialise neural network
+    m_nn = new neural_network(shape, activate_types, loss_type);
 }
+
+// dtor
+MNIST::~MNIST()
+{
+    delete m_nn;
+}
+
+// draw MNIST character and label to output
+void MNIST::print_char(const std::string& set, const int& n)
+{
+    std::cout << "\n";
+    for (int i = 0; i < 28; ++i)
+    {
+        std::cout << "  ";
+        for (int j = 0; j < 28; ++j)
+        {
+            if (set == "train")
+            {
+                if (m_train_x[n][28*i + j] < 0.3)
+                    if (i == 0 || j == 0 || i == 27 || j == 27)
+                        std::cout << "# ";
+                    else
+                        std::cout << "  ";
+                else if (m_train_x[n][28*i + j] < 0.6)
+                    std::cout << ". ";
+                else
+                    std::cout << "# ";
+            }
+            else if (set == "test")
+            {
+                if (m_test_x[n][28*i + j] < 0.3)
+                    std::cout << "# ";
+                else if (m_test_x[n][28*i + j] < 0.6)
+                    std::cout << ". ";
+                else
+                    std::cout << "# ";
+            }
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n    ";
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << "\n    ";
+    for (int i = 0; i < 10; ++i)
+    {
+        if (set == "train" && m_train_y[n][i] == 1.0)
+            std::cout << "^ ";
+        else if (set == "test" && m_test_y[n][i] == 1.0)
+            std::cout << "^ ";
+        else
+            std::cout << "  ";
+    }
+    std::cout << "\n  ";
+}
+
+void MNIST::eval(const std::string& set, const int& n)
+{
+    m_nn->fwd_pass(m_train_x[n]);
+    std::cout << m_nn->get_output() << std::endl;
+}
+
+
 
 /******************************************************************************************************************/
 
 
 int main()
 {   
-    std::tuple<uint8_t*, std::vector<uint32_t>, uint32_t, uint32_t> import_dat = read_IDX<uint8_t>("./data/train-images.idx3-ubyte");
+    MNIST mnist("./data/train-images.idx3-ubyte", 
+        "./data/train-labels.idx1-ubyte",
+        "./data/t10k-images.idx3-ubyte",
+        "./data/t10k-labels.idx1-ubyte", 
+        {28*28, 10, 10}, {"relu", "softmax"}, "crossentropy");
+
+    mnist.print_char("train", 1);
+
+    mnist.eval("train", 1);
+    
 }
