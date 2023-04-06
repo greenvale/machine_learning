470fbd588b838ce4ea2652d18f25a1a541f060c6
William Denny <wdenny1999@gmail.com>
2023-04-06 01:18:31 +0100

    Added nvec_sum

--- computational_graph/computational_graph.hpp
+++ computational_graph/computational_graph.hpp
@@ -16,147 +16,229 @@
 namespace gv
 {
 
-    namespace cg
-    {
+namespace cg
+{
 
-        /* Operation node base class
-        */
-        class operation
-        {
-        public:
-            std::vector<const std::vector<double>*> vp_x;
-            std::vector<std::vector<double>> m_dx;
-            std::vector<double> m_y;
-            std::vector<const std::vector<double>*> vp_dy;
+/* Operation node base class
+*/
+class operation
+{
+public:
+    std::vector<const std::vector<double>*> vp_x;
+    std::vector<std::vector<double>> m_dx;
+    std::vector<double> m_y;
+    std::vector<const std::vector<double>*> vp_dy;
 
-            operation();
-            operation(const int& npar, const int& nchild);
+    operation();
+    operation(const int& npar, const int& nchild);
 
-            virtual void comp() = 0;
-            virtual void grad() = 0;
-            std::vector<double> get_Dy();
-            void input_vec(const std::vector<double>* pvec, const int& ind);
-        };
+    virtual void comp() = 0;
+    virtual void grad() = 0;
+    std::vector<double> get_Dy();
+    void input_vec(const std::vector<double>* pvec, const int& ind);
+};
 
-        // default ctor
-        operation::operation()
-        {
-        }
+// default ctor
+operation::operation()
+{
+}
 
-        // parameter ctor taking number of parent nodes & number of child nodes
-        // initialises containers for inputs, outputs, gradients
-        operation::operation(const int& npar, const int& nchild)
-        {
-            assert(npar > 0 && nchild >= 0);
+// parameter ctor taking number of parent nodes & number of child nodes
+// initialises containers for inputs, outputs, gradients
+operation::operation(const int& npar, const int& nchild)
+{
+    assert(npar > 0 && nchild >= 0);
 
-            m_dx = std::vector<std::vector<double>>(npar);
-            vp_x = std::vector<const std::vector<double>*>(npar);
-            vp_dy = std::vector<const std::vector<double>*>(nchild);
-        }
+    m_dx = std::vector<std::vector<double>>(npar);
+    vp_x = std::vector<const std::vector<double>*>(npar);
+    vp_dy = std::vector<const std::vector<double>*>(nchild);
+}
 
-        // calculates total derivative for DJ/Dy 
-        // DJ/Dy = sum_i{ dJ/d(y_i) }
-        std::vector<double> operation::get_Dy()
-        {
-            // assert that dy container not empty and each element has an equal size > 0
-            assert(vp_dy.size() > 0 && vp_dy[0]->size() > 0);
-            for (auto p : vp_dy)
-                assert(p != nullptr && p->size() == vp_dy[0]->size());
+// calculates total derivative for DJ/Dy 
+// DJ/Dy = sum_i{ dJ/d(y_i) }
+std::vector<double> operation::get_Dy()
+{
+    // assert that dy container not empty and each element has an equal size > 0
+    assert(vp_dy.size() > 0 && vp_dy[0]->size() > 0);
+    for (auto p : vp_dy)
+        assert(p != nullptr && p->size() == vp_dy[0]->size());
 
-            // initialise Dy vector as zero vector
-            std::vector<double> Dy(vp_dy[0]->size(), 0);
+    // initialise Dy vector as zero vector
+    std::vector<double> Dy(vp_dy[0]->size(), 0);
 
-            // accumulate each d(y_i) into Dy vector
-            for (auto p : vp_dy)
-                std::transform(Dy.cbegin(), Dy.cend(), p->cbegin(), Dy.begin(), std::plus<>());
+    // accumulate each d(y_i) into Dy vector
+    for (auto p : vp_dy)
+        std::transform(Dy.cbegin(), Dy.cend(), p->cbegin(), Dy.begin(), std::plus<>());
 
-            return Dy;
-        }
+    return Dy;
+}
 
-        // bind input vector by ptr to operation node given input index
-        void operation::input_vec(const std::vector<double>* pvec, const int& ind)
-        {
-            // assert input index is valid
-            assert(ind >= 0 && ind < vp_x.size());
+// bind input vector by ptr to operation node given input index
+void operation::input_vec(const std::vector<double>* pvec, const int& ind)
+{
+    // assert input index is valid
+    assert(ind >= 0 && ind < vp_x.size());
 
-            vp_x[ind] = pvec;
-        }
+    vp_x[ind] = pvec;
+}
 
-        // binds 2 operations together in series in parent-child relationship
-        // parent output (y) -> child input (x)
-        // takes output index for parent and input index for child
-        void bind(const std::pair<operation*, int>& par_y, const std::pair<operation*, int>& child_x)
-        {
-            // assert that parent and child ptrs point to valid operation with > 0 inputs/outputs and valid indexes
-            assert(par_y.first != nullptr && child_x.first != nullptr);
-            assert(par_y.first->vp_dy.size() > 0 && child_x.first->vp_x.size() > 0);
-            assert(par_y.second >= 0 && par_y.second < par_y.first->vp_dy.size());
-            assert(child_x.second >= 0 && child_x.second < child_x.first->vp_x.size());
+// binds 2 operations together in series in parent-child relationship
+// parent output (y) -> child input (x)
+// takes output index for parent and input index for child
+void bind(const std::pair<operation*, int>& par_y, const std::pair<operation*, int>& child_x)
+{
+    // assert that parent and child ptrs point to valid operation with > 0 inputs/outputs and valid indexes
+    assert(par_y.first != nullptr && child_x.first != nullptr);
+    assert(par_y.first->vp_dy.size() > 0 && child_x.first->vp_x.size() > 0);
+    assert(par_y.second >= 0 && par_y.second < par_y.first->vp_dy.size());
+    assert(child_x.second >= 0 && child_x.second < child_x.first->vp_x.size());
 
-            // parent dy ptr = child dx ptr
-            par_y.first->vp_dy[par_y.second] = &(child_x.first->m_dx[child_x.second]);
+    // parent dy ptr = child dx ptr
+    par_y.first->vp_dy[par_y.second] = &(child_x.first->m_dx[child_x.second]);
 
-            // child x ptr = parent y ptr
-            child_x.first->vp_x[child_x.second] = &(par_y.first->m_y);
-        }
+    // child x ptr = parent y ptr
+    child_x.first->vp_x[child_x.second] = &(par_y.first->m_y);
+}
 
-        //**************************************************************************************************************************
+//**************************************************************************************************************************
 
-        /* Unary operation given by lambda function
-        * Input : x (vector)
-        * Output : y (vector)
-        * y_i = lambda(x_i)
-        * (dJ/dx)_i = lambda'(x_i, y_i)
-        * Lambda function is unary so node has 1 input parent. Lambda derivative is binary taking input vector and output vector.
-        */
-        class unary : public operation
-        {
-        public:
-            int m_size;
-            std::function<double(double)> m_fcn;
-            std::function<double(double, double)> m_fcn_grad;
+/* Unary operation given by lambda function
+* Input : x (vector)
+* Output : y (vector)
+* y_i = lambda(x_i)
+* (dJ/dx)_i = lambda'(x_i, y_i)
+* Lambda function is unary so node has 1 input parent. Lambda derivative is binary taking input vector and output vector.
+*/
+class unary : public operation
+{
+public:
+    int m_size;
+    std::function<double(double)> m_fcn;
+    std::function<double(double, double)> m_fcn_grad;
 
-            unary(const int& nchild, const int& size, std::function<double(double)> fcn,
-                std::function<double(double, double)> fcn_grad);
+    unary(const int& nchild, const int& size, std::function<double(double)> fcn,
+        std::function<double(double, double)> fcn_grad);
 
-            void comp() override;
-            void grad() override;
-        };
+    void comp() override;
+    void grad() override;
+};
 
-        // parameter ctor
-        unary::unary(const int& nchild, const int& size, std::function<double(double)> fcn,
-            std::function<double(double, double)> fcn_grad) : operation(1, nchild)
-        {
-            assert(size > 0);
+// parameter ctor
+unary::unary(const int& nchild, const int& size, std::function<double(double)> fcn,
+    std::function<double(double, double)> fcn_grad) : operation(1, nchild)
+{
+    assert(size > 0);
 
-            m_size = size;
-            m_fcn = fcn;
-            m_fcn_grad = fcn_grad;
+    m_size = size;
+    m_fcn = fcn;
+    m_fcn_grad = fcn_grad;
 
-            m_y = std::vector<double>(m_size);
-            m_dx[0] = std::vector<double>(m_size);
-        }
+    m_y = std::vector<double>(m_size);
+    m_dx[0] = std::vector<double>(m_size);
+}
 
-        // node computation
-        void unary::comp()
-        {
-            assert(vp_x[0] != nullptr);
+// node computation
+void unary::comp()
+{
+    assert(vp_x[0] != nullptr);
 
-            std::transform(vp_x[0]->cbegin(), vp_x[0]->cend(), m_y.begin(), m_fcn);
-        }
+    std::transform(vp_x[0]->cbegin(), vp_x[0]->cend(), m_y.begin(), m_fcn);
+}
 
-        // node gradient
-        void unary::grad()
-        {
-            assert(vp_dy.size() > 0);
-            for (auto p : vp_dy)
-                assert(p != nullptr && p->size() == m_size);
+// node gradient
+void unary::grad()
+{
+    assert(vp_dy.size() > 0);
+    for (auto p : vp_dy)
+        assert(p != nullptr && p->size() == m_size);
 
-            std::vector<double> Dy = get_Dy();
-            std::transform(m_dx[0].cbegin(), m_dx[0].cend(), Dy.cbegin(), m_dx[0].begin(),
-                [](double dx_i, double Dy_i) {return Dy_i * dx_i; });
-        }
+    std::vector<double> Dy = get_Dy();
+    std::transform(m_dx[0].cbegin(), m_dx[0].cend(), Dy.cbegin(), m_dx[0].begin(),
+        [](double dx_i, double Dy_i) {return Dy_i * dx_i; });
+}
 
-    } // namespace cg
+//**************************************************************************************************************************
+
+/* Summation of n vectors (n >= 2)
+* Input : { x_1, ... , x_n } (vectors)
+* Output : y (vector)
+* y = x_1 + ... + x_n
+* dJ/d(x_i) = sum_j{ dJ/d(y_j) } * dy/dx = DJ/Dy * dy/dx = DJ/Dy
+*/
+class nvec_sum : public operation
+{
+public:
+    int m_size;
+
+    nvec_sum(const int& npar, const int& nchild, const int& size);
+
+    void comp() override;
+    void grad() override;
+};
+
+// parameter ctor
+nvec_sum::nvec_sum(const int& npar, const int& nchild, const int& size)
+{
+    assert(size > 0);
+
+    m_size = size;
+    m_y = std::vector<double>(m_size);
+    std::fill(m_dx.begin(), m_dx.end(), std::vector<double>(m_size));
+}
+
+// node computation
+void nvec_sum::comp()
+{
+    assert(vp_x.size() > 0);
+    for (auto p : vp_x)
+        assert(p != nullptr && p->size() == m_size);
+
+    // set y to zero vector
+    std::fill(m_y.begin(), m_y.end(), 0);
+
+    // accumulate sums in vector
+    for (auto p : vp_x)
+        std::transform(m_y.cbegin(), m_y.cend(), p->cbegin(), m_y.begin(), std::plus<>());
+}
+
+// node gradients
+void nvec_sum::grad()
+{
+    for (auto p : vp_dy)
+        assert(p != nullptr && p->size() == m_size);
+    
+    // get total derivative DJ/Dy
+    std::vector<double> Dy = get_Dy();
+
+    for (int i = 0; i < m_dx.size(); ++i)
+        std::copy(Dy.cbegin(), Dy.cend(), m_dx[i].begin());
+}
+
+//**************************************************************************************************************************
+
+/*  Matrix-vector multiplication
+* Input : W (matrix) , x (vector)
+* Output : y (vector)
+* y = W * x
+* dJ/dW = sum_j{ dJ/d(y_j) } * dy/dW
+* = sum_j{ dJ/d(y_j) } * 3d_array( dy_<ROW> / dW_<LANE><COL> )
+* (dJ/dW)_ij = (DJ/Dy)_i * x_j
+* dJ/dx = sum_j{ dJ/d(y_j) } * dy/dx
+* = sum_j{ dJ/d(y_j) } * W
+* (dJ/dx)_i = sum_j{ (DJ/Dy)_j * W_ji }
+*/
+class matvec_mul : public operation
+{
+public:
+    int m_xsize, m_ysize;
+
+    matvec_mul();
+    matvec_mul(const int& nchild, const int& xsize, const int& ysize);
+
+    void comp() override;
+    void grad() override;
+};
+
+} // namespace cg
 
 } // namespace gv
